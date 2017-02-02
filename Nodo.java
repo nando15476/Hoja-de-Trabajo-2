feff189af3d01102e3b6b6ceadde2f527fa816ae
DanMorSch28 <mor15526@uvg.edu.gt>
2017-02-02 11:16:30 -0600

    Add files via upload

---
+++ Nodo.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Listas;
+
+/**
+ *
+ * @author OscarIvan
+ */
+public class Nodo {
+    protected int  Valor; 
+    protected Nodo Siguiente; 
+    
+    public Nodo(int valor)
+    {
+        this.Valor = valor;
+    }
+    
+    public void setValor(int valor)
+    {
+        this.Valor = valor;
+    }
+    
+    public int getValor()
+    {
+        return this.Valor;
+    } 
+    
+    public void setSiguiente(Nodo siguiente)
+    {
+        this.Siguiente = siguiente;
+    }
+    
+    public Nodo getSiguiente()
+    {
+        return this.Siguiente;
+    }
+    
+    public String toString()
+    {
+        return Integer.toString(Valor);
+    }
+            
+}
